daeaa71ca943769f20fca64b241ae0e31758f224
Pitmairen <progrper@gmail.com>
2015-03-11 11:29:13 +0100

    Added base.js

---
+++ js/base.js
@@ -0,0 +1,26 @@
+
+
+function Position(x, y)
+{
+    this.x = x;
+    this.y = y;
+}
+
+
+function GameObject(){
+
+    this.pos = new Position(0, 0);
+
+
+    this.draw = function(ctx){
+
+    };
+
+    this.update = function(timedelta){
+
+
+    };
+
+}
+
+
