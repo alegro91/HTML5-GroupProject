36189fcd2818e9b3f9c053ad709240954747ac02
Pitmairen <progrper@gmail.com>
2015-03-12 10:23:37 +0100

    Add event handler

--- js/base.js
+++ js/base.js
@@ -4,6 +4,7 @@ function Position(x, y)
 {
     this.x = x;
     this.y = y;
+
 }
 
 
@@ -21,6 +22,47 @@ function GameObject(){
 
     };
 
+    this.getRealCoordinates = function(ctx){
+        return {x: this.pos.x, y: ctx.canvas.height - this.pos.y};
+    };
 }
 
+
+
+function InputEvents(){
+
+    var KEYCODE_MAP = {
+        39 : "moveright",
+        37 : "moveleft",
+        38 : "jump",
+    };
+
+    var _event_handlers = {};
+
+    this.on = function(event, handler){
+
+        _event_handlers[event] = handler;
+
+    };
+
+
+    document.addEventListener("keydown", function(event){
+        
+        var code = event.which;
+        if (!KEYCODE_MAP.hasOwnProperty(code))
+            return;
+
+        var ev = KEYCODE_MAP[code];
+
+        if (!_event_handlers.hasOwnProperty(ev))
+            return;
+
+        _event_handlers[ev]();
+
+    });
+    
+
+
+
+}
 
