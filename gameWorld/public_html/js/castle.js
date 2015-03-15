159a3f00485cf09d59b460e5b854762371af8e04
Pitmairen <progrper@gmail.com>
2015-03-15 20:36:49 +0100

    Fixed castle padding and removed unused variables

--- gameWorld/public_html/js/castle.js
+++ gameWorld/public_html/js/castle.js
@@ -6,16 +6,13 @@ function Castle() {
     GameObject.call(this);
     this.pos.x = 400;
     this.pos.y = 50;
-    var castleRadius = 50;
+
     var _this = this;
 
-    var _moving = false;
-    var _groundDecel = 20;
-
-    this.padding.left = castleRadius;
-    this.padding.right = castleRadius;
-    this.padding.bottom = castleRadius + 70;
-    this.padding.top = castleRadius;
+    this.padding.left = 0;
+    this.padding.right = 120;
+    this.padding.bottom = 120;
+    this.padding.top = 0;
 
     this.draw = function (context) {
         var pos = this.getRealCoordinates(context);
